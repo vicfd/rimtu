010fd7e4173150e631dd68b682ae9a13137ac431
Victor <vicfd@hotmail.com>
2015-02-12 12:40:23 +0100

    Ejemplo Mostrar
    Ejemplo Mostrar

--- system/script/download_report.php
+++ system/script/download_report.php
@@ -1,15 +1,5 @@
 <?php
 	include_once '../class.php';
-	
-	if(isset($_GET['id']) && isset($_GET['type']) && isset($_GET['mail']) && isset($_GET['reason']))
-	{
-		$id = replaceStr("%"," ",injection($_GET['id']));
-		$type = replaceStr("%"," ",injection($_GET['type']));
-		$mail = replaceStr("%"," ",injection($_GET['mail']));
-		$reason = replaceStr("%"," ",injection($_GET['reason']));
-
-		$db->InsertDb("reporte","archivo,tipo,correo,razon","'$id','$type','$mail','$reason'");
-		html_function("El reporte ha sido registrado. Le reenviaremos de nuevo a la pagina anterior");
-		jsredireccionar("goMylove('#container','../system/class/download.php?id=$id')",3000);
+	ve('#container','../system/class/download.php?id=$id')",3000);
 	}
 ?>
--- system/script/function_disconnect.php
+++ system/script/function_disconnect.php
@@ -4,16 +4,24 @@
 	if (isset($_POST["disconnect"])) 
 	{
 		session_unset();
-		session_destroy();
-		$_SESSION['usuario'] = "anonimo";
-		$_SESSION['nivel'] = 0;
-		html_function("Acaba de desconectarse, sera redireccionado a la pagina principal.");
-		echo
-		'
-			<script type="text/javascript">
-				goMylove(\'#head\',\'../system/class/head.php\');
-				setTimeout(function(){goMylove(\'#container\',\'../system/class/home.php\',\'../index.html\')},2000);
-			</script>
-		';
-	}
-?>
+
+	if(isset($_GET['id']) && isset($_GET['type']) && isset($_GET['mail']) && isset($_GET['reason']))
+	{
+		$id = replaceStr("%"," ",injection($_GET['id']));
+		$type = replaceStr("%"," ",injection($_GET['type']));
+		$mail = replaceStr("%"," ",injection($_GET['mail']));
+		$reason = replaceStr("%"," ",injection($_GET['reason']));
+
+		$db->InsertDb("reporte","archivo,tipo,correo,razon","'$id','$type','$mail','$reason'");
+		html_function("El reporte ha sido registrado. Le reenviaremos de nuevo a la pagina anterior");
+		jsredireccionar("goMylo
+	if(isset($_GET['id']) && isset($_GET['type']) && isset($_GET['mail']) && isset($_GET['reason']))
+	{
+		$id = replaceStr("%"," ",injection($_GET['id']));
+		$type = replaceStr("%"," ",injection($_GET['type']));
+		$mail = replaceStr("%"," ",injection($_GET['mail']));
+		$reason = replaceStr("%"," ",injection($_GET['reason']));
+
+		$db->InsertDb("reporte","archivo,tipo,correo,razon","'$id','$type','$mail','$reason'");
+		html_function("El reporte ha sido registrado. Le reenviaremos de nuevo a la pagina anterior");
+		jsredireccionar("goMylo
